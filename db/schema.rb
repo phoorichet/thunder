7ab28215afee83ce54da5d6ecd5c24654d46aa20
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2015-05-01 02:39:18 +0700

    Added Book model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429173652) do
+ActiveRecord::Schema.define(version: 20150430193850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(version: 20150429173652) do
     t.integer  "insured_user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "begin_at"
+    t.datetime "end_at"
+    t.string   "number"
+    t.string   "description"
+    t.string   "status"
+    t.string   "book_type"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "insured_users", force: :cascade do |t|
