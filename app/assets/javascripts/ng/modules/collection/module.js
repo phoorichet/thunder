70f2e5eaebfc1e7f226d723fc307d331288cc594
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2015-05-27 01:18:53 +0700

    remove unused module

--- app/assets/javascripts/ng/modules/collection/module.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function(){
-
-  angular.module('shoppes.collection', [ 'shoppes.collection.services', 
-  																			 'shoppes.collection.controllers',
-                                         'shoppes.api.services', ]);
-  
-}());
--- app/assets/javascripts/ng/modules/item/module.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-(function(){
-
-  angular.module('shoppes.item', [ 'shoppes.item.services', 
-                                   'shoppes.api.services', ]);
-  
-}());
